37a4eddc5dd820a85b81d040e6d5771ef08ad111
Yode <yode0419@gmail.com>
2023-08-07 13:25:18 +0800

    架構

--- script.js
+++ script.js
@@ -1,52 +1,110 @@
 const speedElement = document.getElementById('speed');
 const highestSpeedElement = document.getElementById('highestSpeed');
 const resetButton = document.getElementById('resetButton');
-const cube2 = document.getElementById('cube2');
-let previousTimestamp = null;
+const speedText = document.getElementById('speedText'); //修改文字顏色大小
+const speedTextExtra = document.getElementById('speedTextExtra'); //額外增加文字
+
 let scrollSpeed = 0;
 let highestSpeed = 0;
+let animationFrameId = null;
 
-function calculateScrollSpeed(event) {
-    if (previousTimestamp !== null) {
-        const currentTimestamp = performance.now();
-        const deltaTime = currentTimestamp - previousTimestamp;
+function SpeedDown() {
+    speedElement.textContent = Math.round(scrollSpeed);
 
-        // 檢查deltaTime是否太小，避免出現"infinity"
-        if (deltaTime > 5) {
-            const scrollDistance = Math.abs(event.deltaY);
-            scrollSpeed = Math.round(scrollDistance / deltaTime * 100) / 100;
-            speedElement.innerText = scrollSpeed + ' pixels/ms';
-
-            if (scrollSpeed > highestSpeed) {
-                highestSpeed = scrollSpeed;
-                highestSpeedElement.innerText = highestSpeed + ' pixels/ms';
-            }
-        }
-        updateAnimationDuration();
+    // 更新speedText的樣式
+    if (scrollSpeed >= 0 && scrollSpeed < 1000) {
+        speedText.style.color = 'black';
+        speedText.style.fontSize = '1rem';
+        speedTextExtra.style.display='none';
+    } else if (scrollSpeed >= 1000 && scrollSpeed < 5000) {
+        speedText.style.color = 'blue';
+        speedText.style.fontSize = '1.25rem';
+        speedTextExtra.style.display='none';
+    } else if (scrollSpeed >= 5000 && scrollSpeed < 10000) {
+        speedText.style.color = 'red';
+        speedText.style.fontSize = '1.5rem';
+        speedTextExtra.style.display='none';
+    } else if (scrollSpeed >= 10000) {
+        speedText.style.color = 'orange';
+        speedText.style.fontSize = '3rem';
+        // 顯示額外的文字
+        speedTextExtra.style.display='block';
+        speedTextExtra.style.color = 'orange';
+        speedTextExtra.style.fontSize = '3rem';
+        speedTextExtra.textContent = '乾 燒起來辣!';
     }
-    previousTimestamp = performance.now();
+
+    if (scrollSpeed > 1) {
+        // 逐漸減少滾動速度
+        scrollSpeed *= 0.99;
+        animationFrameId = requestAnimationFrame(SpeedDown);
+    } else {
+        // 清除定時器，滾動速度接近0
+        cancelAnimationFrame(animationFrameId);
+    }
 }
 
-function resetHighestSpeed() {
-    highestSpeed = 0;
-    highestSpeedElement.innerText = highestSpeed + ' pixels/ms';
+
+function handleScroll(event) {
+    let delta = 0;
+    if (event.type === 'touchmove') {
+        // 在手機上使用手指觸控滑動，可以使用 event.touches[0] 來獲取觸控信息
+        delta = event.touches[0].clientY - event.touches[0].screenY;
+    } else {
+        // 在電腦上使用滑鼠滾輪滾動操作，可以使用 event.deltaY 獲取滾動方向和滾動速度
+        delta = event.deltaY || event.detail || event.wheelDelta;
+    }
+
+    // 累加滾動的速度
+    scrollSpeed += Math.abs(delta);
+
+    // 更新最高滾動像素量
+    if (scrollSpeed > highestSpeed) {
+        highestSpeed = scrollSpeed;
+        highestSpeedElement.textContent = Math.round(highestSpeed);
+    }
+
+    // 防止滾動時觸發預設行為，例如頁面滾動
+    event.preventDefault();
+
+    // 重置定時器
+    cancelAnimationFrame(animationFrameId);
+    // 啟動定時器來逐漸減少滾動速度
+    animationFrameId = requestAnimationFrame(SpeedDown);
+}
+
+function resetSpeed() {
     scrollSpeed = 0;
+    highestSpeed = 0;
+    speedElement.innerText = scrollSpeed;
+    highestSpeedElement.innerText = highestSpeed;
 }
 
-function updateAnimationDuration() {
-    const animationDuration = 10 * Math.abs((1 - scrollSpeed / 100) ** 4);
-    cube2.style.animationDuration = `${animationDuration}s`;
+// 為 touchmove 事件添加監聽器，這樣在手機上可以支持手指觸控滑動
+document.addEventListener('touchmove', handleScroll, {
+    passive: false
+});
 
-    //    console.log(animationDuration);
-    //    console.log(cube2.style.animationDuration);
+// 為滾動事件添加監聽器，這樣在電腦上可以繼續使用滑鼠滾輪滾動操作
+if ('onwheel' in document) {
+    // 支持現代瀏覽器
+    document.addEventListener('wheel', handleScroll, {
+        passive: false
+    });
+} else if ('onmousewheel' in document) {
+    // 支持舊版IE
+    document.addEventListener('mousewheel', handleScroll, {
+        passive: false
+    });
+} else {
+    // Firefox
+    document.addEventListener('DOMMouseScroll', handleScroll, {
+        passive: false
+    });
 }
 
-window.addEventListener('wheel', calculateScrollSpeed);
-resetButton.addEventListener('click', resetHighestSpeed);
-
-cube2.onclick = function () {
-    cube2.style.animationDuration = `0s`;
-};
+// 歸零按鈕點事件擊監聽器
+resetButton.addEventListener('click', resetSpeed);
 
 
 
@@ -85,7 +143,6 @@ const renderer = new THREE.WebGLRenderer({
 
 // enabling shadows
 renderer.shadowMap.enabled = true;
-//renderer.shadowMap.type = THREE.BasicShadowMap;
 
 //{antialias: true, alpha: true}
 // 初始渲染畫面尺寸
@@ -93,10 +150,6 @@ renderer.setSize(container.clientWidth, container.clientHeight);
 
 // 加入 canvas 元素供渲染畫面
 container.appendChild(renderer.domElement);
-
-//// 設定軌道控制器，讓相機可以環繞觀察場景
-//const controls = new OrbitControls(camera, renderer.domElement);
-//controls.update();
 
 // 產生平面物體
 const planeGeometry = new THREE.PlaneGeometry(6, 6);
@@ -147,10 +200,20 @@ function animate() {
     }
     // 設定正方形轉動效果
     //
-    cube.rotation.x += 0.01 + scrollSpeed / 100;
+    cube.rotation.x += scrollSpeed / 10000;
 
     renderer.render(scene, camera);
 }
 
 // 開始執行動畫
 animate();
+
+// 配合視窗大小自動更新
+function onWindowResize() {
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener('resize', onWindowResize);
